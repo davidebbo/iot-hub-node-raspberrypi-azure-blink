841015fad1635308f04cdd66f794255130f12216
ZhijunZhao <zhijun.zhao@outlook.com>
2016-09-13 15:56:01 +0800

    Rework sample code (#3)
    * rework sample code
    
    * minor change
    
    * cleanup
    
    * fix typo
    
    * cleanup
    
    * minor change

--- az-blink.js
+++ az-blink.js
@@ -1,38 +1,50 @@
 'use strict';
 
-var config = require('./config.json');
-var clientFromConnectionString = require('azure-iot-device-amqp').clientFromConnectionString;
 var wpi = require('wiring-pi');
-
-var connectionString = 'HostName=' + config.iot_hub_host_name + ';DeviceId=' + config.iot_hub_device_id + ';SharedAccessKey=' + config.iot_hub_device_key;
-var client = clientFromConnectionString(connectionString);
+var config = require('./config.json');
+var Message = require('azure-iot-device').Message;
+var clientFromConnectionString = require('azure-iot-device-amqp').clientFromConnectionString;
+var deviceConnectionString = 'HostName=' + config.iot_hub_host_name + ';DeviceId=' + config.iot_hub_device_id + ';SharedAccessKey=' + config.iot_hub_device_key;
 
 // GPIO pin of the LED
 var CONFIG_PIN = 7;
 
+var MAX_BLINK_TIMES = 20;
+var totalBlinkTimes = 0;
+
 wpi.setup('wpi');
 wpi.pinMode(CONFIG_PIN, wpi.OUTPUT);
 var isLedOn = false;
-
-function printResultFor(op) {
-  return function printResult(err, res) {
-    if (err) console.log(op + ' error: ' + err.toString());
-    if (res) console.log(op + ' status: ' + res.constructor.name);
-  };
-}
 
 var connectCallback = function (err) {
   if (err) {
     console.log('Could not connect: ' + err);
   } else {
     console.log('Client connected');
-    client.on('message', function (msg) {
-      console.log('Id: ' + msg.messageId + ' Body: ' + msg.data);
-      client.complete(msg, printResultFor('completed'));
-      isLedOn = !isLedOn;
-      wpi.digitalWrite(CONFIG_PIN, isLedOn ? 1 : 0);
-    });
+
+    sendMessageAndBlink();
   }
 };
 
+function sendMessageAndBlink(){
+  var message = new Message(JSON.stringify({ deviceId: config.iot_hub_device_id, messageId: totalBlinkTimes }));
+  console.log("Sending message: " + message.getData());
+  client.sendEvent(message, sendMessageCallback);
+
+  // Blink while sending each message.
+  isLedOn = !isLedOn;
+  wpi.digitalWrite(CONFIG_PIN, isLedOn ? 1 : 0);
+}
+
+function sendMessageCallback(err, res) {
+  if (err) console.log('Send message error: ' + err.toString());
+  if (res) console.log('Send message status: ' + res.constructor.name);
+
+  if(totalBlinkTimes < MAX_BLINK_TIMES){
+    totalBlinkTimes++;
+    setTimeout(sendMessageAndBlink, 2000);
+  }
+}
+
+var client = clientFromConnectionString(deviceConnectionString);
 client.open(connectCallback);
