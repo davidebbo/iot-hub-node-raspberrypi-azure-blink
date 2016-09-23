c59834e4c70aadf817c98514445589aa220b65ec
FAREAST\zhijzhao <zhijzhao@microsoft.com>
2016-09-23 19:22:13 +0800

    Add IoT Hub reading function and Azure table reading function

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,11 @@
-﻿require('gulp-common')(require('gulp'), 'raspberrypi-node-az-blink');
+﻿var args = require('get-gulp-args')();
+
+var mainFunc = args['read-storage'] ? require('./azure-table.js').readAzureTable : require('./iot-hub.js').readIoTHub;
+var callback = args['read-storage'] ? require('./azure-table.js').callback : require('./iot-hub.js').callback;
+
+var options = {
+  main: mainFunc,
+  callback: callback,
+};
+
+require('gulp-common')(require('gulp'), 'raspberrypi-node-az-blink', options);
